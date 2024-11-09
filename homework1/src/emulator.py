6c272dcf9c7618ce1dded2b8f93991c2eb8ec1c2
Rashid Yusubov <rashidyusubov@yandex.ru>
2024-11-09 22:40:49 +0300

    Initial class structure and project setup

--- homework1/src/emulator.py
+++ homework1/src/emulator.py
@@ -3,3 +3,12 @@ import json
 import tarfile
 import datetime
 from pathlib import PurePosixPath
+
+class ShellEmulator:
+    def __init__(self, username, fs_path, log_path):
+        self.username = username
+        self.fs_path = fs_path
+        self.log_path = log_path
+        self.current_dir = '/'
+        self.log_data = []
+        self._file_system = {self.current_dir: []}  # Инициализация корневой директории
