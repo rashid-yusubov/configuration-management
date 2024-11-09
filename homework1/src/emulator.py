240c4a269563cd633ed2246784179109607f06c7
Rashid Yusubov <rashidyusubov@yandex.ru>
2024-11-09 22:41:31 +0300

    Implement loading of virtual file system from tar archive

--- homework1/src/emulator.py
+++ homework1/src/emulator.py
@@ -12,3 +12,20 @@ class ShellEmulator:
         self.current_dir = '/'
         self.log_data = []
         self._file_system = {self.current_dir: []}  # Инициализация корневой директории
+
+def load_filesystem(self):
+    """Загружает виртуальную файловую систему (архив tar)"""
+    with tarfile.open(self.fs_path, mode="r") as tar:
+        for member in tar.getmembers():
+            member.name = "/" + member.name  # гарантируем, что путь начинается с /
+            member_path = member.name.split("/")
+            if len(member_path) == 2:
+                self._file_system["/"].append(member.name)
+                self._file_system[member.name] = []
+                continue
+
+            parent = "/".join(member_path[:-1])
+            if parent not in self._file_system:
+                self._file_system[parent] = []
+            self._file_system[parent].append(member.name)
+            self._file_system[member.name] = []
