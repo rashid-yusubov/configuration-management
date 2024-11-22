f52293a009fff91b53327c0c771fc4e7ca5c1ef6
Rashid Yusubov <rashidyusubov@yandex.ru>
2024-11-22 04:55:12 +0300

    Update main.py

--- homework2/src/main.py
+++ homework2/src/main.py
@@ -1,8 +1,9 @@
-import yaml
 import os
 import subprocess
-from git_analyzer import get_commit_by_tag
+import yaml
 from graph_builder import build_dependency_graph, build_plantuml
+from git_analyzer import get_commit_by_tag
+
 
 def main(config_path):
     # Чтение конфигурации с указанием кодировки
@@ -19,21 +20,31 @@ def main(config_path):
     visualization_tool_path = config.get("visualization_tool_path")
 
     # Получение стартового коммита
-    start_commit = get_commit_by_tag(repo_path, tag_name)
+    try:
+        start_commit = get_commit_by_tag(repo_path, tag_name)
+    except ValueError as e:
+        print(e)
+        return
 
     # Построение графа зависимостей
     graph = build_dependency_graph(repo_path, start_commit)
+    if not graph:
+        print("Не удалось построить граф зависимостей.")
+        return
 
     # Создание PlantUML
     plantuml_code = build_plantuml(graph)
 
     # Сохранение результата
-    with open(output_file, "w", encoding="utf-8") as file:  # Указание кодировки
-        file.write(plantuml_code)
-
-    print(f"Граф зависимостей успешно создан. Сохранён в {output_file}.")
-    print("\n--- PlantUML Graph ---\n")
-    print(plantuml_code)  # Вывод в консоль
+    try:
+        with open(output_file, "w", encoding="utf-8") as file:  # Указание кодировки
+            file.write(plantuml_code)
+        print(f"Граф зависимостей успешно создан. Сохранён в {output_file}.")
+        print("\n--- PlantUML Graph ---\n")
+        print(plantuml_code)  # Вывод в консоль
+    except Exception as e:
+        print(f"Ошибка при сохранении файла {output_file}: {e}")
+        return
 
     # Если указан путь к инструменту визуализации, выполнить его
     if visualization_tool_path:
@@ -42,14 +53,3 @@ def main(config_path):
             print(f"Граф визуализирован с использованием {visualization_tool_path}.")
         except Exception as e:
             print(f"Ошибка при вызове PlantUML: {e}")
-
-if __name__ == "__main__":
-    import sys
-
-    # Путь по умолчанию для файла конфигурации
-    default_config_path = os.path.join(os.path.dirname(__file__), "..", "config", "config.yaml")
-
-    # Использование пути по умолчанию, если не указан другой путь
-    config_path = sys.argv[1] if len(sys.argv) > 1 else default_config_path
-
-    main(config_path)
