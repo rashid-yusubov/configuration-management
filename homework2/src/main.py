7ee3fba7f0bbb9ca6047661707304f6de77a64ca
Rashid Yusubov <rashidyusubov@yandex.ru>
2024-11-22 04:57:08 +0300

    Update main.py

--- homework2/src/main.py
+++ homework2/src/main.py
@@ -1,8 +1,8 @@
 import os
-import subprocess
 import yaml
-from graph_builder import build_dependency_graph, build_plantuml
+import subprocess
 from git_analyzer import get_commit_by_tag
+from graph_builder import build_dependency_graph, build_plantuml
 
 
 def main(config_path):
@@ -29,22 +29,25 @@ def main(config_path):
     # Построение графа зависимостей
     graph = build_dependency_graph(repo_path, start_commit)
     if not graph:
-        print("Не удалось построить граф зависимостей.")
-        return
+        print("Граф зависимостей пуст.")
+    else:
+        print("Граф зависимостей построен успешно.")
 
     # Создание PlantUML
     plantuml_code = build_plantuml(graph)
+    if not plantuml_code:
+        print("Не удалось создать код для PlantUML.")
+    else:
+        print("\n--- PlantUML Graph ---\n")
+        print(plantuml_code)  # Вывод в консоль
 
     # Сохранение результата
     try:
         with open(output_file, "w", encoding="utf-8") as file:  # Указание кодировки
             file.write(plantuml_code)
         print(f"Граф зависимостей успешно создан. Сохранён в {output_file}.")
-        print("\n--- PlantUML Graph ---\n")
-        print(plantuml_code)  # Вывод в консоль
     except Exception as e:
         print(f"Ошибка при сохранении файла {output_file}: {e}")
-        return
 
     # Если указан путь к инструменту визуализации, выполнить его
     if visualization_tool_path:
@@ -53,3 +56,15 @@ def main(config_path):
             print(f"Граф визуализирован с использованием {visualization_tool_path}.")
         except Exception as e:
             print(f"Ошибка при вызове PlantUML: {e}")
+
+
+if __name__ == "__main__":
+    import sys
+
+    # Путь по умолчанию для файла конфигурации
+    default_config_path = os.path.join(os.path.dirname(__file__), "..", "config", "config.yaml")
+
+    # Использование пути по умолчанию, если не указан другой путь
+    config_path = sys.argv[1] if len(sys.argv) > 1 else default_config_path
+
+    main(config_path)
