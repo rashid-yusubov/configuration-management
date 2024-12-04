f4029d7bcb46e66d8d1e1bac8f944d1bc99fbb5d
Rashid Yusubov <rashidyusubov@yandex.ru>
2024-12-04 15:43:26 +0300

    Update main.py

--- homework3/src/main.py
+++ homework3/src/main.py
@@ -8,12 +8,18 @@ class ConfigLanguageConverter:
         self.constants = {}
 
     def parse_yaml(self, yaml_input):
+        """
+        Парсинг YAML.
+        """
         try:
             return yaml.safe_load(yaml_input)
         except yaml.YAMLError as e:
             raise ValueError(f"Ошибка парсинга YAML: {e}")
 
     def to_config_language(self, data):
+        """
+        Преобразует данные в конфигурационный язык.
+        """
         if isinstance(data, dict):
             return self.convert_dict(data)
         elif isinstance(data, list):
@@ -26,9 +32,12 @@ class ConfigLanguageConverter:
             raise ValueError(f"Недопустимый тип данных: {type(data)}")
 
     def convert_dict(self, data):
+        """
+        Преобразует словарь в формат { key = value }.
+        """
         result = ["{"]
         for key, value in data.items():
-            if not re.match(r'^[_a-zA-Z]+$', key):
+            if not re.match(r'^[_a-zA-Z][_a-zA-Z0-9]*$', key):
                 raise ValueError(f"Недопустимое имя: {key}")
             value_repr = self.to_config_language(value)
             result.append(f" {key} = {value_repr}")
@@ -36,21 +45,33 @@ class ConfigLanguageConverter:
         return "\n".join(result)
 
     def convert_list(self, data):
+        """
+        Преобразует массив в формат '( value value ... )'.
+        """
         values = " ".join(self.to_config_language(value) for value in data)
         return f"'( {values} )"
 
     def define_constant(self, name, value):
-        if not re.match(r'^[_a-zA-Z]+$', name):
+        """
+        Объявляет константу.
+        """
+        if not re.match(r'^[_a-zA-Z][_a-zA-Z0-9]*$', name):
             raise ValueError(f"Недопустимое имя для константы: {name}")
         self.constants[name] = value
 
     def evaluate_constant(self, name):
+        """
+        Вычисляет значение константы.
+        """
         if name not in self.constants:
             raise ValueError(f"Неопределенная константа: {name}")
         return self.constants[name]
 
 
 def main():
+    """
+    Основной метод для запуска.
+    """
     input_data = sys.stdin.read()
     converter = ConfigLanguageConverter()
 
